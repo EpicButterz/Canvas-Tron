548338d916681bfa016236bb1f37a78ecfa7a80f
Eric Butterfield <ewbutterfield@gmail.com>
2013-11-16 14:47:53 -0700

    added tail collision, changed color of tail, added reset function

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var canvas, ctx, sprites,
+var canvas, ctx,
 width = 960,
 height = 540,
 rightKey = false,
@@ -17,44 +17,93 @@ player2 = {
 	x: 900,
 	y: 500
 };
+function reset(){
+	player1 = {
+		width: 25,
+		height: 25,
+		x: 20,
+		y: 20
+	};
+	ctx.clearRect(0,0,900,500);
+	rightKey = false;
+	leftKey = false;
+	upKey = false;
+	downKey = false;
+} 
 
 function clearCanvas() {
 	ctx.clearRect(player1.x,player1.y,25,25);
 }
 
 function drawPlayers() {
+	var pixTop = ctx.getImageData(player1.x, player1.y - 1, 25, 1).data;  /// location to sample a pixel from
+	// ctx.fillRect(player1.x, player1.y - 7, 25, 1);
+	var pixBottom = ctx.getImageData(player1.x, player1.y + 27, 25, 1).data;
+	// ctx.fillRect(player1.x, player1.y + 40, 25, 1);
+	var pixLeft = ctx.getImageData(player1.x - 2, player1.y, 1, 25).data;
+	// ctx.fillRect(player1.x - 2, player1.y + 7, 1, 1);
+	var pixRight = ctx.getImageData(player1.x + 27, player1.y, 1, 25).data;
+	// ctx.fillRect(player1.x + 27, player1.y + 7, 1, 1)
+	/// check that it is the color of the background (here transparent)
+	if (pixTop[3] !== 0) {
+    	console.log("You Lose!");
+    	reset();
+	 }else if (pixBottom[3] !== 0){
+	 	console.log("You Lose!")
+	 	reset();
+	 }else if (pixLeft[3] !== 0){
+	 	console.log("You Lose!")
+	 	reset();
+	 }else if (pixRight[3] !== 0){
+	 	console.log("You Lose!")
+	 	reset();
+	 }
+
 	if (rightKey) {
 		//Collision with East wall
 		if(player1.x > 930){
 			player1.x += 0;
-			console.log('WALL!');
-		}else{
-			// If no collision continue
-			ctx.fillRect(player1.x,player1.y + 12,15,3);
+			console.log('You Lose!');
+			reset();
+		}else{// If no collision continue
+			// Draw tail
+			ctx.fillStyle = "#8ed0de";
+			ctx.fillRect(player1.x,player1.y + 12,3,3);
+			ctx.fillStyle = "black";
+			// Move player forward
 			player1.x += 5;
 		}
 	} else if (leftKey) {		
 		if(player1.x < 0){
 			player1.x -= 0;
-			console.log('WALL!');
+			console.log('You Lose!');
+			reset();
 		}else{
-			ctx.fillRect(player1.x + 15,player1.y + 12,15,3);
+			ctx.fillStyle = "#8ed0de";
+			ctx.fillRect(player1.x + 25,player1.y + 12,3,3);
+			ctx.fillStyle = "black";
 			player1.x -= 5;
 		}
 	} else if (upKey){
 		if(player1.y < 0){
 			player1.y -= 0;
-			console.log('WALL!');
+			console.log('You Lose!');
+			reset();
 		}else{
-			ctx.fillRect(player1.x + 10,player1.y + 10,3,15);
+			ctx.fillStyle = "#8ed0de";
+			ctx.fillRect(player1.x + 12,player1.y + 25,3,3);
+			ctx.fillStyle = "black";
 			player1.y -= 5;
 		}
 	}	else if (downKey){
 		if(player1.y > 525){
 			player1.y -= 0;
-			console.log('WALL!');
+			console.log('You Lose!');
+			reset();
 		}else{
-			ctx.fillRect(player1.x + 10,player1.y - 5,3,15);
+			ctx.fillStyle = "#8ed0de";
+			ctx.fillRect(player1.x + 12,player1.y,3,3);
+			ctx.fillStyle = "black";
 			player1.y += 5;
 		}
 	}
@@ -62,14 +111,14 @@ function drawPlayers() {
 	ctx.fillRect(player2.x,player2.y,player2.width,player2.height);
     	
 	  // Are the players touching?
-	if (
-		player1.x <= (player2.x + 25)
-		&& player2.x <= (player1.x + 25)
-		&& player1.y <= (player2.y + 25)
-		&& player2.y <= (player1.y + 25)
-		) {
-		console.log('Player!');
-	}
+	// if (
+	// 	player1.x <= (player2.x + 25)
+	// 	&& player2.x <= (player1.x + 25)
+	// 	&& player1.y <= (player2.y + 25)
+	// 	&& player2.y <= (player1.y + 25)
+	// 	) {
+	// 	console.log('Player!');
+	// }
 }
 
 function loop() {
